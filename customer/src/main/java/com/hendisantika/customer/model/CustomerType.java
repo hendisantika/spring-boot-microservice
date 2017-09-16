31adce61206635ad904e7a572b91b30b280082dd
Hendi Santika <hendisantika@gmail.com>
2017-09-16 19:05:36 +0700

    #10 - Added enum CustomerType

---
+++ customer/src/main/java/com/hendisantika/customer/model/CustomerType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.customer.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : customer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/16/17
+ * Time: 7:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum CustomerType {
+    INDIVIDUAL, COMPANY;
+}
