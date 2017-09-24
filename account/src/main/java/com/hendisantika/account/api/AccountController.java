39e3fbf25e652b8535e90ac9be05f6a69b94bf8d
Hendi Santika <hendisantika@gmail.com>
2017-09-24 14:05:35 +0700

    #21 - Added more controller in Account Controller

--- account/src/main/java/com/hendisantika/account/api/AccountController.java
+++ account/src/main/java/com/hendisantika/account/api/AccountController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.account.api;
 
 import com.hendisantika.account.model.Account;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+
+    @RequestMapping("/{id}")
+    public Account findAccountById(@PathVariable("id") Integer id) {
+        return new Account(id, 1, "123456789", 1234);
+    }
+
+    @RequestMapping("/customer/{customerId}")
+    public Account findAccountByCustomerId(@PathVariable("customerId") Integer customerId) {
+        return new Account(1, customerId, "123456789", 1234);
+    }
 
     @RequestMapping("/")
     public Account findAccount() {
