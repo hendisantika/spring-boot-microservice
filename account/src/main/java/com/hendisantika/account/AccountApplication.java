b061529b291a9fe58ee258be00ebd067bad3ce1f
Hendi Santika <hendisantika@gmail.com>
2017-09-25 07:50:18 +0700

    #28 - Modified Main App - Account Service

--- account/src/main/java/com/hendisantika/account/AccountApplication.java
+++ account/src/main/java/com/hendisantika/account/AccountApplication.java
@@ -2,8 +2,12 @@ package com.hendisantika.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,5 +18,9 @@ public class AccountApplication {
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
-
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().url("jdbc:mysql://localhost:3306/oauth2?createDatabaseIfNotExist=true&useSSL=false")
+				.username("default").password("default").driverClassName("com.mysql.jdbc.Driver").build();
+	}
 }
