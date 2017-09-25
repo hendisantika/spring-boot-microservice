f1a3a29a516ed501d2158dbe5ca9fc8594253435
Hendi Santika <hendisantika@gmail.com>
2017-09-25 08:01:08 +0700

    #33 - Added SecurityConfig - Gateway

---
+++ gateway/src/main/java/com/hendisantika/gateway/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.hendisantika.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-microservice
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/25/17
+ * Time: 8:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableWebSecurity
+@Order(-10)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and().httpBasic().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+//		auth.inMemoryAuthentication().withUser("root").password("password").roles("USER");
+    }
+}
