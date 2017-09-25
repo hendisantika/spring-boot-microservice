a5566a567f0d79b7d7d880749b4666baf81f3252
Hendi Santika <hendisantika@gmail.com>
2017-09-25 07:11:42 +0700

    #27 - Modified auth service

--- auth/src/main/java/com/hendisantika/auth/AuthApplication.java
+++ auth/src/main/java/com/hendisantika/auth/AuthApplication.java
@@ -2,18 +2,22 @@ package com.hendisantika.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.security.Principal;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableDiscoveryClient
 @EnableResourceServer
+//@EnableJdbcHttpSession
 @RestController
 public class AuthApplication {
 
@@ -24,5 +28,11 @@ public class AuthApplication {
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
 		return user;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().url("jdbc:mysql://localhost:3306/oauth2?useSSL=false")
+				.username("default").password("default").driverClassName("com.mysql.jdbc.Driver").build();
 	}
 }
