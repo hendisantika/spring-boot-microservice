ac73dd0c1f0267960c2e919c35343a647455d180
Hendi Santika <hendisantika@gmail.com>
2017-09-25 07:59:56 +0700

    #32 - Modified Main App - Gateway

--- gateway/src/main/java/com/hendisantika/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/hendisantika/gateway/GatewayApplication.java
@@ -2,15 +2,29 @@ package com.hendisantika.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableOAuth2Sso
 @EnableDiscoveryClient
+@EnableJdbcHttpSession
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().url("jdbc:mysql://localhost:3306/oauth2?useSSL=false")
+				.username("default").password("default").driverClassName("com.mysql.jdbc.Driver").build();
 	}
 }
